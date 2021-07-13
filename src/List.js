578f47e0be9eb90300cbee86e916c908ef23650a
Abdulrahman <boodynaser@rocketmail.com>
2021-07-14 00:19:51 +0200

    Center Todo List and Add User Input

--- src/List.js
+++ src/List.js
@@ -4,7 +4,8 @@ export function CreatList(Task) {
 // eslint-disable-next-line guard-for-in
   for (const i in Task) {
     const NewListItem = document.createElement('li');
+    NewListItem.setAttribute('id', 'ListItem');
     NewListItem.innerHTML = Task[i].description;
-    document.getElementById('ListItems').appendChild(NewListItem);
+    document.getElementById('ListContainer').appendChild(NewListItem);
   }
-};
+}
