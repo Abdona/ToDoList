342c40099cf09f0a3222747cf336db82c221fa5f
Abdulrahman <boodynaser@rocketmail.com>
2021-07-14 01:11:25 +0200

    Fix linter Errors

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    index: './src/index.js'
+    index: './src/index.js',
   },
   devServer: {
     contentBase: './dist',
