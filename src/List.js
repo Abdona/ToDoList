8620a1e84726087bcdaa9086d3a524a66757776c
Abdulrahman <boodynaser@rocketmail.com>
2021-07-14 13:21:29 +0200

    Complete List Styling

--- src/List.js
+++ src/List.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-restricted-syntax */
+import MyImage from './images/3-vertical-dots.svg';
 // eslint-disable-next-line import/prefer-default-export
 export function CreatList(Task) {
 // eslint-disable-next-line guard-for-in
@@ -6,10 +7,15 @@ export function CreatList(Task) {
     const NewListItem = document.createElement('li');
     const NewListTask = document.createElement('p');
     const NewListCheck = document.createElement('input');
+    const NewListdots = document.createElement('img');
+    const TaskDiv = document.createElement('div');
+    TaskDiv.setAttribute('id', 'taskcont');
+    NewListdots.setAttribute('src', MyImage);
     NewListItem.setAttribute('id', 'ListItem');
     NewListCheck.setAttribute('type', 'checkbox');
     NewListTask.innerHTML = Task[i].description;
     document.getElementById('ListContainer').appendChild(NewListItem).appendChild(NewListCheck);
-    document.getElementById('ListContainer').appendChild(NewListItem).appendChild(NewListTask);
+    document.getElementById('ListContainer').appendChild(NewListItem).appendChild(TaskDiv).appendChild(NewListTask);
+    document.getElementById('ListContainer').appendChild(NewListItem).appendChild(NewListdots);
   }
 }
