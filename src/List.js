ad7a9597174815fd2aa5eae9d12a7ce7c08517c0
Abdulrahman <boodynaser@rocketmail.com>
2021-07-14 11:28:20 +0200

    Add more Styles

--- src/List.js
+++ src/List.js
@@ -4,8 +4,12 @@ export function CreatList(Task) {
 // eslint-disable-next-line guard-for-in
   for (const i in Task) {
     const NewListItem = document.createElement('li');
+    const NewListTask = document.createElement('p');
+    const NewListCheck = document.createElement('input');
     NewListItem.setAttribute('id', 'ListItem');
-    NewListItem.innerHTML = Task[i].description;
-    document.getElementById('ListContainer').appendChild(NewListItem);
+    NewListCheck.setAttribute('type', 'checkbox');
+    NewListTask.innerHTML = Task[i].description;
+    document.getElementById('ListContainer').appendChild(NewListItem).appendChild(NewListCheck);
+    document.getElementById('ListContainer').appendChild(NewListItem).appendChild(NewListTask);
   }
 }
